1fef328a9374afb8da72822e2ca8b097e2f0b1eb
Neha <reachojhaneha@gmail.com>
2018-06-15 13:40:38 -0400

    bfs1

--- BreadthFirsttree.java
+++ BreadthFirsttree.java
@@ -4,33 +4,29 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class BreadthFirsttree {
-	
+
 	public BreadthFirsttree() {
-	BinaryTree b1 = new BinaryTree();
-	Node BreadthNode = b1.getroot();
+		BinaryTree b1 = new BinaryTree();
+		Node BreadthNode = b1.getroot();
 	}
-	Queue<Integer> q1 = new LinkedList<Integer>();
-	
+
+	Queue<Node> q1 = new LinkedList<Node>();
+
 	public void bfs(Node BreadthNode) {
 		Node first = BreadthNode;
-		if (first != null) {
-			if(q1.contains(first.getdata())) {
-				
-			q1.remove(first.getdata());
+		q1.add(first);
+		while (!q1.isEmpty()) {
+			Node temp = q1.peek();
+			q1.remove();
+			System.out.println(temp.getdata());
+			if (temp.getleftChild() != null) {
+				q1.add(temp.getleftChild());
 			}
-			System.out.println(first.getdata());
-			q1.add(first.getdata());
-			/*System.out.println(first.getleftChild());
-			//q1.add(first.getleftChild().getdata());
-			System.out.println(first.getrightChild());
-			//q1.add(first.getrightChild().getdata());
-*/			Node leftchild = first.getleftChild();
-			bfs(leftchild);
-			Node rightchild= first.getrightChild();
-			bfs(rightchild);
+			if (temp.getrightChild() != null) {
+				q1.add(temp.getrightChild());
+			}
 		}
+
 	}
-	
-	
 
 }
